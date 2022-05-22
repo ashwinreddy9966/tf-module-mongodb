a4a6e8e21f40b35c4d8a4aa410c5292e9afac7c9
manu <manu@local.com>
2022-05-22 20:41:58 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -9,9 +9,9 @@ resource "aws_docdb_cluster" "docdb" {
 
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-${var.ENV}"
-  subnet_ids = [aws_subnet.private.*.id]
+  subnet_ids = data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS
 
   tags = {
-    Name = "My docdb subnet group"
+    Name = "roboshop-subnet-group-${var.ENV}"
   }
 }
