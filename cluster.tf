2c4eb4576c76e6c235d2739352523eef32c8e467
manu <manu@local.com>
2022-05-24 11:04:26 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -18,7 +18,7 @@ resource "aws_docdb_subnet_group" "docdb" {
  }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
+  count              = variable.DCOUMENTDB_PORT
   identifier         = "roboshop-${var.ENV}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = "db.t3.medium"
