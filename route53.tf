3687f50f6ed077d015334606f0302328ff4421b6
manu <manu@local.com>
2022-05-24 09:43:35 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "record" {
   zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-  name    = ""
+  name    = "mongodb-${var.ENV}-$"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_docdb_cluster.docdb.endpoint]
