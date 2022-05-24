6eff471f1fd332890d7d0ebe015decc768555fa4
manu <manu@local.com>
2022-05-24 09:41:02 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "record" {
   zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-  name    = ""
+  name    = "roboshop-docdb-dev.cluster-ciqb6xvrbz0q.us-east-1.docdb.amazonaws.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_eip.lb.public_ip]
