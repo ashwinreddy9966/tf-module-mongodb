5b517cb48f93bff4b593e0c8ff0ba6f013856bd4
manu <manu@local.com>
2022-05-24 11:54:42 +0530

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -11,6 +11,10 @@ data "aws_secretsmanager_secret" "secret" {
   name = "${var.ENV}/roboshop/secrets"
 }
 
+data "aws_secretsmanager_secret_version" "secret-version" {
+  secret_id = data.aws_secretsmanager_secret.example.id
+}
+
 output "data" {
   value = data.aws_secretsmanager_secret.secret
 }
