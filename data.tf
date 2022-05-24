f531a18232e791f9bb1f04c4cca5b7c82aa29521
manu <manu@local.com>
2022-05-24 12:07:10 +0530

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -21,5 +21,4 @@ data "aws_secretsmanager_secret_version" "secrets" {
 
 output "data" {
   value = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCUMENTDB_MASTER_USERNAME"]
-  sensitive = false
 }
